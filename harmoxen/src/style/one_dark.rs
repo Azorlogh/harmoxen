c7930e4a8dbb21b65bd65fea982d46f9604b7d97
Azorlogh <bott.alix@gmail.com>
2021-03-24 14:21:32 +0100

    fix wrapping

--- harmoxen/src/style/one_dark.rs
+++ harmoxen/src/style/one_dark.rs
@@ -53,7 +53,7 @@ impl button::StyleSheet for Button {
 
 	fn hovered(&self) -> button::Style {
 		button::Style {
-			background: BG_1.into(),
+			background: BG_3.into(),
 			..self.active()
 		}
 	}
